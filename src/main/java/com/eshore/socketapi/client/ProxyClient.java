8b7d8525ba601cabfecb941067eeb592a69cdcae
eric <shmilyhe@163.com>
2019-04-11 17:21:17 +0800

    添加客户端关闭功能

--- src/main/java/com/eshore/socketapi/client/ProxyClient.java
+++ src/main/java/com/eshore/socketapi/client/ProxyClient.java
@@ -1,4 +1,5 @@
 package com.eshore.socketapi.client;
+import java.io.Closeable;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -7,7 +8,7 @@ import java.net.UnknownHostException;
 import com.eshore.socketapi.commons.TunnelOverSnappyProtocol;
 import com.eshore.socketapi.commons.TunnelProtocol;
 import com.eshore.socketapi.server.*;
-public class ProxyClient {
+public class ProxyClient implements Closeable{
 	//static IProtocol sproto=new TunnelProtocol();
 	static TunnelProtocol sproto=new TunnelOverSnappyProtocol();
 	//static IProtocol dproto=new RawProtocol();
@@ -18,9 +19,9 @@ public class ProxyClient {
 	public ProxyClient(String ip,int port,GlobWorker gw) throws UnknownHostException, IOException{
 		this(ip,port,gw,sproto);
 	}
-	
+	Socket s ;
 	public ProxyClient(String ip,int port,GlobWorker gw,TunnelProtocol p) throws UnknownHostException, IOException{
-		Socket s = new Socket(ip,port);
+		s = new Socket(ip,port);
 		ClientHandler handle= new ClientHandler(gw,p);
 		server =new TunnelClientWorker( s, handle,p,gw);
 		//server.setId("default");
@@ -37,5 +38,15 @@ public class ProxyClient {
 	 */
 	public boolean isLogon(){
 		return server.isLogon();
+	}
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		server.close();
+		if(s!=null){
+			s.close();
+		}
+		
 	}
 }
